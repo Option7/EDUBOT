3e09fd3402e6bba844483222cae7eecea6416d3c
Option7 <34314511+Option7@users.noreply.github.com>
2022-02-05 18:51:05 +0000

    Add files via upload

---
+++ EDUBOT/examples/Robotics/single_wheel/single_wheel.ino
@@ -0,0 +1,12 @@
+#include <EDUBOT.h>
+
+MOTOR motorA(11, 13);
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+motorA.forward();
+}
